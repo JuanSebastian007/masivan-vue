ec25d34542ca808e81c7190a1e3810c33d518b28
Sebastian Quino <sebastian.quino@imagineapps.co>
2020-10-22 18:47:37 -0500

    Se obtiene la informacion del api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
+  data: function() {
+    return {
+      maxNumber: 0,
+      comicsCalificationsNumber: [],
+      comic: {}
+    };
+  },
   name: "Home",
-  components: {
-    HelloWorld
+  async mounted() {
+    this.$comicsCalificationsNumber = [];
+    this.$maxNumber = await this.getMaxNumber().then(res => {
+      return res;
+    });
+    // this.getComicNumber(this.$maxNumber);
+    this.getComic(this.$maxNumber);
+  },
+  methods: {
+    getMaxNumber() {
+      return new Promise((resolve, reject) => {
+        axios
+          .get("http://localhost:8080/info.0.json")
+          .then(res => {
+            if (res) {
+              resolve(res.data.num);
+            }
+          })
+          .catch(() => {
+            reject(0);
+          });
+      });
+    },
+    getComicNumber(maxNumber) {
+      let comicNumber = Math.floor(Math.random() * maxNumber);
+      do {
+        comicNumber = Math.floor(Math.random() * maxNumber);
+      } while (this.$comicsCalificationsNumber.includes(comicNumber));
+      return comicNumber;
+    },
+    async getComic(maxNumber) {
+      let comicNumber = this.getComicNumber(maxNumber);
+      this.$comic = await axios
+        .get(`http://localhost:8080/${comicNumber}/info.0.json`)
+        .then(res => {
+          return res.data;
+        });
+    }
   }
 };
 </script>
